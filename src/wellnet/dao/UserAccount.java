a245efa6c992f0d480325e84f7b4257c223b70cb
dakinGithub <dakin.development@gmail.com>
2015-07-17 01:55:43 -0500

    Updated UserAccount.java after accidental deletion :)

--- src/wellnet/dao/UserAccount.java
+++ src/wellnet/dao/UserAccount.java
@@ -5,33 +5,30 @@ public class UserAccount {
 	
 	private int userId = 0;
 	private String username = "";
+	private String pswd = "";
+	private String pswdSalt = "";
 	private int accountId = 0;
-	private String companyName = "";
-	private String address = "";
-	private String phone = "";
-	private String email = "";
-	private boolean approved = false;
-	private AccountType accountType = new AccountType();
 	
+	public static String[] ColumnNames = {
+		"USER_ID",
+		"USERNAME",
+		"ENCRYPTED_PASSWORD",
+		"PASSWORD_SALT",
+		"ACCOUNT_ID"
+	};
+	
+
 	public UserAccount(){
 		
 	}
 		
-	public UserAccount(int userId, String username, int accountId, String companyName,
-			String address, String phone, String email, char approved, AccountType accountType) {
+	public UserAccount(int userId, String username, String pswd, String pswdSalt, int accountId) {
 
 		this.userId = userId;
 		this.username = username;
+		this.pswd = pswd;
+		this.pswdSalt = pswdSalt;
 		this.accountId = accountId;
-		this.companyName = companyName;
-		this.address = address;
-		this.phone = phone;
-		this.email = email;
-		this.accountType = accountType;
-		
-		if(approved == 'Y' || approved == 'y'){
-			this.approved = true;			
-		}
 		
 	}
 	
@@ -47,51 +44,23 @@ public class UserAccount {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	public String getPswdSalt() {
+		return pswdSalt;
+	}
+	public void setPswdSalt(String pswdSalt) {
+		this.pswdSalt = pswdSalt;
+	}
 	public int getAccountId() {
 		return accountId;
 	}
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public boolean isApproved() {
-		return approved;
-	}
-	public void setApproved(boolean approved) {
-		this.approved = approved;
-	}
-	public AccountType getAccountType() {
-		return accountType;
-	}
-	public void setAccountType(AccountType accountType) {
-		this.accountType = accountType;
-	}
-
-	 
-	
-	
 	
 }
